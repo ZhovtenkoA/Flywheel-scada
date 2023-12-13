8d5cd948b1990e7f5066d2c74cc01e8cb46ea91e
ZhovtenkoA <127408550+ZhovtenkoA@users.noreply.github.com>
2023-12-13 11:32:35 +0200

    Update modbus_main.py

--- modbus_main.py
+++ modbus_main.py
@@ -908,7 +908,7 @@ def write_time():
 
 def convert_VDC(adc):
     V = adc * 0.01893310546 # V = (ADC*23.5*3.3) / 4096
-    V = round(V, 6)
+    V = round(V, 2)
     return V
 
 def resize_window(event):
