c03786ccb1535e616d758c112cc59cb671b15265
ZhovtenkoA <127408550+ZhovtenkoA@users.noreply.github.com>
2023-12-18 14:01:23 +0200

    Update modbus_main.py

--- modbus_main.py
+++ modbus_main.py
@@ -249,10 +249,12 @@ def read_holding_30001_30014():
                             converted_value = convert_VDC(vdc = registers[i])
                             output_fields[i].delete(1.0, END)
                             output_fields[i].insert(END, f"{converted_value}")
+
                         elif i == 13:
                             converted_adc = convert_ADC(adc = registers[i])
                             output_fields[i].delete(1.0, END)
                             output_fields[i].insert(END, f"{converted_adc}")
+                            
                         else:    
                             output_fields[i].delete(1.0, END)
                             output_fields[i].insert(END, f"{registers[i]}")
