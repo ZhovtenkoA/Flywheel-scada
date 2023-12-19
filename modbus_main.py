ca9b8e1de4ec9da0307712fdbd6121bef2ec02b1
ZhovtenkoA <127408550+ZhovtenkoA@users.noreply.github.com>
2023-12-19 14:37:26 +0200

    Update modbus_main.py

--- modbus_main.py
+++ modbus_main.py
@@ -844,8 +844,8 @@ def make_kW_h():
     )
         start_time = datetime.now()
         end_time = start_time + timedelta(seconds=5)
+        n = 6
         while datetime.now() < end_time:
-            n = 6
             kW_power_output.delete(1.0, END)
             kW_power_output.insert(END, f"{n - 1}s ")
             try:
